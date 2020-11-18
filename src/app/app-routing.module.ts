fe5dcc4aac460c9481a795cbc32334bf5758dcb6
Carryall <carryall369@gmail.com>
2020-11-18 12:09:17 +0700

    add login screen component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginScreenComponent } from './login-screen/login-screen.component';
+
+const routes: Routes = [
+  { path: '', component: LoginScreenComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
