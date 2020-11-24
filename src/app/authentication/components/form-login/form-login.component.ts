c8bf3e274d0c6df66b10ca9fa62484dd2ee804b3
Carryall <carryall369@gmail.com>
2020-11-24 10:51:08 +0700

    rename component selector

--- src/app/authentication/components/form-login/form-login.component.ts
+++ src/app/authentication/components/form-login/form-login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'form-login',
+  selector: 'app-form-login',
   templateUrl: './form-login.component.html',
   styleUrls: ['./form-login.component.scss']
 })
