78c696ffde5c330c8c60d488055e8b8810c1867f
Carryall <carryall369@gmail.com>
2020-11-24 10:23:56 +0700

    rename page folder

--- src/app/authentication/authentication-routing.module.ts
+++ src/app/authentication/authentication-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
  
-import { LoginComponent } from './screens';
+import { LoginComponent } from './pages';
  
 const routes: Routes = [
   { path: '', component: LoginComponent }
--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,7 +1,6 @@
-
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
-import { LoginComponent } from './screens';
+import { LoginComponent } from './pages';
 import { FormLoginComponent } from './components/form-login/form-login.component';
 
 @NgModule({
