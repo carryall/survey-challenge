e7e661025ee4adc009d33c979a1c8e00468638d8
Carryall <carryall369@gmail.com>
2020-11-26 11:54:20 +0700

    remove extra space after function

--- src/app/authentication/pages/login/login.component.ts
+++ src/app/authentication/pages/login/login.component.ts
@@ -7,9 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
