c4152606d7003720b5d3425ea0e0e881234968c6
Carryall <carryall369@gmail.com>
2020-11-30 17:48:56 +0700

    fix lint

--- src/app/shared/services/session.service.ts
+++ src/app/shared/services/session.service.ts
@@ -3,13 +3,14 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+
 export class SessionService {
   USER_ACCESS_TOKEN = 'user_access_token';
   USER_TOKEN_TYPE = 'user_token_type';
 
   constructor() { }
 
-  setAccessToken(accessToken: string, tokenType: string) {
+  setAccessToken(accessToken: string, tokenType: string): void {
     localStorage.setItem(this.USER_ACCESS_TOKEN, accessToken);
     localStorage.setItem(this.USER_TOKEN_TYPE, tokenType);
   }
