df4077032fad694365c517cb0fb49cb608dae6fa
Carryall <carryall369@gmail.com>
2020-12-10 16:11:42 +0700

    use key value error messages

--- src/app/authentication/components/form-login/form-login.component.ts
+++ src/app/authentication/components/form-login/form-login.component.ts
@@ -10,6 +10,10 @@ import { AuthService } from 'src/app/shared/services/auth.service';
   styleUrls: ['./form-login.component.scss']
 })
 export class FormLoginComponent implements OnInit {
+  ERROR_MESSAGES: { [key: number]: string; } = {
+    400: 'Invalid email or password'
+  };
+
   loginForm: any;
   errorMessage = '';
 
@@ -40,10 +44,15 @@ export class FormLoginComponent implements OnInit {
         this.router.navigate(['/']);
       },
       error => {
-        if (error.status === 400) {
-          this.errorMessage = 'Invalid email or password';
-        }
+        this.errorMessage = this.errorMessageFor(error.status);
       }
     );
+  }
+
+  private errorMessageFor(status: any): any {
+    if (!Object.keys(this.ERROR_MESSAGES).includes(status.toString())) {
+      return 'Something went wrong, please try again later';
+    }
+    return this.ERROR_MESSAGES[status];
   }
 }
