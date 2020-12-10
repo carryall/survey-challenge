b0af3ba749f8fe22b5a0c1486239bb24849adeda
Carryall <carryall369@gmail.com>
2020-12-10 12:16:25 +0700

    rename environment key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+  isProduction: true,
   apiBaseUrl: process.env.API_BASE_URL,
   apiVersion: process.env.API_VERSION,
   apiClientID: process.env.API_CLIENT_ID,
